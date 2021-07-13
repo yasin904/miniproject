ca7dc9ba623e6cbae1df5f09a2d62a7b1d8f28b2
yasin904 <mohammedyasin1904@gmail.com>
2021-07-13 16:25:47 +0530

    change

---
+++ src/kinetic.c
@@ -0,0 +1,22 @@
+/**
+ * @file kinetic.c
+ * @author mohammed yasin(mohammedyasin1904@gmail.com)
+ * @brief file to calculate the kinetic energy
+ * @version 0.1
+ * @date 2021-07-13
+ * 
+ * @copyright Copyright(c) 2021
+ * 
+ */
+#include "func.h"
+ 
+ void kinetic()
+ {
+  float m,v,k;
+  printf("enter mass and velocity");
+  scanf("%f%f",&m,&v);
+  k=0.5*m*v;
+  printf("kinetic energy is%f",k);
+
+  
+ }
