d59e233174f74743f122bf00fdb934765af0d34b
yasin904 <mohammedyasin1904@gmail.com>
2021-07-14 08:59:37 +0530

    commit

---
+++ 3_Implementation/src/gravitational.c
@@ -0,0 +1,23 @@
+/**
+ * @file gravitational.c
+ * @author mohammed yasin(mohammedyasin1904@gmail.com)
+ * @brief file to calculate the gravitational energy
+ * @version 0.1
+ * @date 2021-07-13
+ * 
+ * @copyright Copyright(c) 2021
+ * 
+ */
+#include "func.h"
+#include<stdio.h>
+ 
+ void gravitational()
+ {
+  float m,h,g;
+  printf("enter mass and height");
+  scanf("%f%f",&m,&h);
+  g=9.8*m*h;
+  printf("gravitational energy is%f",g);
+
+  
+ }
---
+++ 3_Implementation/src/mechanical.c
@@ -0,0 +1,23 @@
+/**
+ * @file gravitational.c
+ * @author mohammed yasin(mohammedyasin1904@gmail.com)
+ * @brief file to calculate the mechanical energy
+ * @version 0.1
+ * @date 2021-07-13
+ * 
+ * @copyright Copyright(c) 2021
+ * 
+ */
+#include "func.h"
+#include<stdio.h>
+ 
+ void mechanical()
+ {
+  float k,g,m;
+  printf("enter kinetic and gravitational energy");
+  scanf("%f%f",&k,&g);
+  m=g+k;
+  printf("mechanical energy is%f",m);
+
+  
+ }
