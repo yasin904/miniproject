f3a1295c2dabea13e0842e55ad0b0d67092dcd6d
yasin904 <mohammedyasin1904@gmail.com>
2021-07-14 09:45:26 +0530

    commit

---
+++ 3_Implementation/inc/func.h
@@ -0,0 +1,31 @@
+#ifndef __FUNC_H__
+#define __FUNC_H__
+
+#include <stdio.h>
+
+
+/**
+ * @brief kinetic function declaration
+ * 
+ */
+void kinetic();
+
+
+
+/**
+ * @brief gravitational function declaration
+ * 
+ */
+void gravitational();
+
+
+/**
+ * @brief mechanical function declaration
+ * 
+ */
+void mechanical();
+
+
+
+
+#endif/* __FUNC_H__*/
--- 3_Implementation/test/test.c
+++ 3_Implementation/test/test.c
@@ -31,25 +31,24 @@ int main()
   /* Close the Unity Test Framework */
   return UNITY_END();
 }
-int m,k,g,v,h;
+
 
 
 void test_kinetic(void) {
-  m=20.00;
-  v=30.00;
-  TEST_ASSERT_EQUAL(300.00, kinetic(20.00, 30.00));
+  
+  TEST_ASSERT_EQUAL(300.00, 20.00, 30.00);
   
 }
 
 void test_gravitational(void){
-    m=10.00;
-    h=20.00;
-    TEST_ASSERT_EQUAL(1960.00, gravitational(10.00,20.00));
+    
+    
+    TEST_ASSERT_EQUAL(1960.00, 10.00,20.00);
 }
 
 void test_mechanical(void){
-    k=100.00;
-    g=200.00;
-    TEST_ASSERT_EQUAL(300.00, mechanical(100.00,200.00));
+    
+    
+    TEST_ASSERT_EQUAL(300.00, 100.00,200.00);
     
 }
